7bec677a1c1a4d3acdaef748c95e0f907c5c545a
Trevor Dally <trevordally23@gmail.com>
2021-09-16 14:52:04 -0500

    Added practical python folder and Ch3 code

---
+++ trevor/practical_python/Ch3/load_display_save.py
@@ -0,0 +1,17 @@
+import cv2 as cv
+import argparse as arg
+
+ap = arg.ArgumentParser()
+ap.add_argument("-i", "--image", required = True,
+    help = "Path to the image")
+args = vars(ap.parse_args())
+
+image = cv.imread(args["image"])
+print("wdith: {} pixels".format(image.shape[1]))
+print("length: {} pixels".format(image.shape[0]))
+print("channels: {}".format(image.shape[2]))
+
+cv.imshow("Image", image)
+cv.waitKey(0)
+
+cv.imwrite("newimage.jpg", image)
